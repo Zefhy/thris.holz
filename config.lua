92cab780f350f7562693c0801239ad4100a3a37f
Nico Copper <lpberlin01@gmail.com>
2019-11-22 15:01:19 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,20 @@
+Config              = {}
+Config.MarkerType   = 30
+Config.DrawDistance = 100.0
+Config.ZoneSize     = {x = 1.0, y = 2.0, z = 1.0}
+Config.MarkerColor  = {r = 2, g = 246, b = 0}
+Config.ShowBlips   = true  -- Blip Mapa
+
+Config.RequiredCopsKoda  = 0
+
+Config.TimeToFarm    = 1 * 1000
+Config.TimeToProcess = 1 * 1000
+Config.TimeToSell    = 1 * 1000
+
+Config.Locale = 'de'
+
+Config.Zones = {
+	ApanhaLaranjas =		{x = -1489.14,	y = 4735.27,	z = 55.35,	name = _U('apanha_de_laranjas'),		sprite = 238,	color = 47},
+	SumoDeLaranja =			{x = -565.61,	y = 5325.68,	z = 73.59,	name = _U('transforma_do_sumo_de_laranja'),	sprite = 499,	color = 47},
+	VendaDeSumo =			{x = 2748.14,	y = 3472.85,	z = 55.67,	name = _U('venda_de_sumo_de_laranja'),		sprite = 500,	color = 47}
+}
---
+++ thyris_holz_items.sql
@@ -0,0 +1,4 @@
+INSERT INTO `items` (name, label, `limit`) VALUES
+	('wood1', 'Baumstamm', 50),
+	('wood2', 'Holzbrett', 10)
+;
